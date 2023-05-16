cd24afa58dbc60e745828cf524617e6d18a2be96
70gaurav <gk17265@gmail.com>
2023-05-16 20:50:20 +0530

    admin signup commit

---
+++ Back/db/index.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const connection = mongoose.connect("mongodb+srv://gaurav:quiz@quiz.xnh4fju.mongodb.net/?retryWrites=true&w=majority" , {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+})
+
+export default connection;                              
---
+++ Back/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import adminRouter from "./routes/admin.js" 
+import connection from "./db/index.js"
+import bodyParser from "body-parser"
+
+const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine" ,"pug")
+
+app.use("/admin",adminRouter)
+
+
+
+connection
+.then(()=> {
+    app.listen(3000,() => console.log("server started"))
+})
+.then((err) => {
+    console.log(err)
+})
