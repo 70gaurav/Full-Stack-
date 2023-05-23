ce6fbe8018136b0ee801083b91c75609659d0487
70gaurav <gk17265@gmail.com>
2023-05-23 17:32:45 +0530

    mongo url commit

--- Back/db/index.js
+++ Back/db/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const connection = mongoose.connect("mongodb+srv://gaurav:quiz@quiz.xnh4fju.mongodb.net/?retryWrites=true&w=majority" , {
+const connection = mongoose.connect("mongodb+srv://ecommerce:12345@cluster.sjamvbz.mongodb.net/?retryWrites=true&w=majority" , {
     useNewUrlParser: true,
     useUnifiedTopology:true,
 })
